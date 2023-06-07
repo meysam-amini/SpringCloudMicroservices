9aee3f07fc2adf66dd1701eeb924f1a532642939
m.amini <m.amini@ernyka.com>
2023-06-07 13:44:33 +0330

    KeycloakService interface

--- backoffice/src/main/java/com/meysam/service/keycloakService/KeycloakServiceImpl.java
+++ backoffice/src/main/java/com/meysam/service/keycloakService/KeycloakServiceImpl.java
@@ -1,4 +1,30 @@
 package com.meysam.service.keycloakService;
 
-public class KeycloakServiceImpl {
+import com.meysam.model.entity.Role;
+import com.meysam.model.entity.User;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class KeycloakServiceImpl implements KeycloakService{
+    @Override
+    public List<Role> getRoles() {
+        return null;
+    }
+
+    @Override
+    public User registerUser(User user) {
+        return null;
+    }
+
+    @Override
+    public Role getUserRole() {
+        return null;
+    }
+
+    @Override
+    public User assignRole(User user) {
+        return null;
+    }
 }
