82130f115430f058d8f2f82506330596e6ccf76d
m.amini <m.amini@ernyka.com>
2023-06-19 19:53:22 +0330

    add refresh-token apis structure

---
+++ common/common-utils/src/main/java/com/meysam/common/utils/web/FeignErrorDecoder.java
@@ -0,0 +1,25 @@
+//package com.meysam.common.utils.web;
+//
+//import org.springframework.http.HttpStatus;
+//import org.springframework.stereotype.Component;
+//import org.springframework.web.server.ResponseStatusException;
+//
+//@Component
+//public class FeignErrorDecoder implements ErrorDecoder {
+//    @Override
+//    public Exception decode(String methodKey, Response response) {
+//        switch (response.status()){
+//            case 400:
+//                //Do...
+//                break;
+//            case 404:
+//                if(methodKey.contains("getAlbums")) {
+//                    return new ResponseStatusException(HttpStatus.valueOf(response.status()),
+//                            "User Albums Not Found!");
+//                }
+//            default:
+//                return new Exception(response.reason());
+//        }
+//        return null;
+//    }
+//}
