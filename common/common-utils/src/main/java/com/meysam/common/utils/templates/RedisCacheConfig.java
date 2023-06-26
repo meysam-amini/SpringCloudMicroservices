a1e1f820002c0402861da107c90485403df037ef
m.amini <m.amini@ernyka.com>
2023-06-26 17:29:06 +0330

    update RedisCacheConfig

--- common/common-utils/src/main/java/com/meysam/common/utils/templates/RedisCacheConfig.java
+++ common/common-utils/src/main/java/com/meysam/common/utils/templates/RedisCacheConfig.java
@@ -1,43 +1,31 @@
 package com.meysam.common.utils.templates;
 
-import com.fasterxml.jackson.annotation.JsonAutoDetect;
-import com.fasterxml.jackson.annotation.PropertyAccessor;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.annotation.CachingConfigurerSupport;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.cache.RedisCacheConfiguration;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.RedisPassword;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisClientConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.StringRedisTemplate;
-import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.RedisSerializationContext;
 
 import java.time.Duration;
 
-/**
- * @author GS
- * @date 2017年12月29日
- */
+
 @Configuration
 @EnableCaching
 public class RedisCacheConfig extends CachingConfigurerSupport {
 
-    @Value("${spring.redis.host}")
+    @Value("${spring.data.redis.host}")
     private String REDIS_HOST;
-    @Value("${spring.redis.port}")
+    @Value("${spring.data.redis.port}")
     private int REDIS_PORT;
 //    @Value("${spring.redis.password}")
 //    private String REDIS_PASSWORD;
-    @Value("${spring.redis.connect-timeout}")
+    @Value("${spring.data.redis.connect-timeout}")
     private Long JEDDIS_CONNECTION_TIME_OUT;
-    @Value("${spring.redis.timeout}")
+    @Value("${spring.data.redis.timeout}")
     private Long JEDDIS_READ_TIME_OUT;
 
 
