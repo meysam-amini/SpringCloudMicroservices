db1b100b238f820842b0a04e837ef39b05f8002a
meysam-amini <meysamamini7394@gmail.com>
2023-06-29 14:06:07 +0330

    add backoffice submodules

--- backoffice/webapi/src/main/java/com/meysam/backoffice/webapi/BackofficeWebApiApplication.java
+++ backoffice/webapi/src/main/java/com/meysam/backoffice/webapi/BackofficeWebApiApplication.java
@@ -1,4 +1,4 @@
-package com.meysam;
+package com.meysam.backoffice.webapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,10 +6,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class BackofficeApplication {
+public class BackofficeWebApiApplication {
 
     public static void main(String []args){
-        SpringApplication.run(BackofficeApplication.class,args);
+        SpringApplication.run(BackofficeWebApiApplication.class,args);
     }
 
 }
