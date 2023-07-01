33611fa74a3f7c11ca6eeb85c43dc049bf5a5860
m.amini <m.amini@ernyka.com>
2023-07-01 11:22:14 +0330

    backoffice poms

--- backoffice/backoffice-webapi/src/main/java/com/meysam/backoffice/webapi/BackofficeWebApiApplication.java
+++ backoffice/backoffice-webapi/src/main/java/com/meysam/backoffice/webapi/BackofficeWebApiApplication.java
@@ -3,14 +3,11 @@ package com.meysam.backoffice.webapi;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableDiscoveryClient
