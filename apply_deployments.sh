0eea27ba9446cf90a3c82695ab0097b81ac4951b
m.amini <m.amini@ernyka.com>
2023-07-10 21:00:19 +0330

    change k8s deployments

---
+++ apply_deployments.sh
@@ -0,0 +1,17 @@
+cd wallet-manager/wallet-webapi/
+kubectl apply -f k8s
+
+cd ../../users/users-webapi/
+kubectl apply -f k8s
+
+cd ../../config-server/
+kubectl apply -f k8s
+
+cd ../backoffice/backoffice-webapi/
+kubectl apply -f k8s
+
+cd ../../auth/auth-webapi/
+kubectl apply -f k8s
+
+cd ../../api-gateway/
+kubectl apply -f k8s
---
+++ build-images.sh
@@ -0,0 +1,17 @@
+cd wallet-manager/wallet-webapi/
+docker build -t wallet-ws .
+
+cd ../../users/users-webapi/
+docker build -t users-ws .
+
+cd ../../config-server/
+docker build -t config-server .
+
+cd ../backoffice/backoffice-webapi/
+docker build -t backoffice-ws .
+
+cd ../../auth/auth-webapi/
+docker build -t auth-ws .
+
+cd ../../api-gateway/
+docker build -t api-gateway .
