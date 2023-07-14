7a83b4a0b9d3f1c876786df27c9e4d19bb9b90f5
meysam-amini <meysamamini7394@gmail.com>
2023-07-14 12:01:12 +0330

    update backoffice bootstrap config

--- build-images.sh
+++ build-images.sh
@@ -1,17 +1,22 @@
-cd wallet-manager/wallet-webapi/
-docker build -t wallet-ws .
+#for registering images on minikube env
+eval $(minikube -p minikube docker-env)
 
-cd ../../users/users-webapi/
-docker build -t users-ws .
+#cd wallet-manager/wallet-webapi/
+#docker build -t wallet-ws .
+#
+#cd ../../users/users-webapi/
+#docker build -t users-ws .
 
-cd ../../config-server/
+#cd ../../config-server/
+cd config-server/
 docker build -t config-server .
 
 cd ../backoffice/backoffice-webapi/
+#cd backoffice/backoffice-webapi/
 docker build -t backoffice-ws .
 
-cd ../../auth/auth-webapi/
-docker build -t auth-ws .
+#cd ../../auth/auth-webapi/
+#docker build -t auth-ws .
 
 cd ../../api-gateway/
 docker build -t api-gateway .
