bff3f7e736120bff213544768213dca8ce6e8ed5
meysam-amini <meysamamini7394@gmail.com>
2023-07-17 03:52:16 +0330

    kubernetes discovery client

---
+++ apply_deployments.sh
@@ -0,0 +1,18 @@
+#cd wallet-manager/wallet-webapi/
+#kubectl apply -f k8s
+
+#cd ../../users/users-webapi/
+#kubectl apply -f k8s
+
+#cd ../../config-server/
+#cd config-server/
+#kubectl apply -f k8s
+
+cd backoffice/backoffice-webapi/
+kubectl apply -f k8s
+
+#cd ../../auth/auth-webapi/
+#kubectl apply -f k8s
+
+#cd ../../api-gateway/
+#kubectl apply -f k8s
