f35522af45df4dd7015c6f49db590debc007e547
meysam-amini <meysamamini7394@gmail.com>
2023-07-31 03:16:53 +0330

    queryDsl problem with spring boot 3+

--- common/common-service/src/main/java/com/meysam/common/service/impl/MemberServiceImpl.java
+++ common/common-service/src/main/java/com/meysam/common/service/impl/MemberServiceImpl.java
@@ -3,22 +3,33 @@ package com.meysam.common.service.impl;
 import com.meysam.common.dao.MemberRepository;
 import com.meysam.common.model.dto.*;
 import com.meysam.common.model.entity.Member;
+import com.meysam.common.model.entity.QMember;
 import com.meysam.common.service.api.AuthServiceClient;
 import com.meysam.common.service.api.MemberService;
 import com.meysam.common.service.api.WalletServiceClient;
 import com.meysam.common.utils.exception.BusinessException;
 import com.meysam.common.utils.messages.LocaleMessageSourceService;
 import com.meysam.common.utils.utils.PredicateUtils;
+import com.querydsl.core.types.Order;
+import com.querydsl.core.types.OrderSpecifier;
 import com.querydsl.core.types.Predicate;
+import com.querydsl.core.types.dsl.PathBuilder;
+import com.querydsl.jpa.impl.JPAQuery;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.math.BigInteger;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Service
@@ -29,6 +40,7 @@ public class MemberServiceImpl implements MemberService {
     private final LocaleMessageSourceService messageSourceService;
     private final WalletServiceClient walletServiceClient;
     private final AuthServiceClient authServiceClient;
+    private final EntityManager entityManager;
 
 
     @Override
@@ -55,9 +67,25 @@ public class MemberServiceImpl implements MemberService {
     public Page<MemberDto> pageQuery(MemberDto memberDto) {
         Predicate predicate = PredicateUtils.getPredicate(memberDto.getBooleanExpressions());
         //if different sort was needed (ex: sort by username) :
-        /*PageRequest pageRequest = PageRequest.of
+        PageRequest pageRequest = PageRequest.of
                 (memberDto.getPageQueryModel().getPageNumber(),
-                        memberDto.getPageQueryModel().getPageSize(), Sort.Direction.ASC,"username");*/
+                        memberDto.getPageQueryModel().getPageSize(), Sort.Direction.ASC,"id");
+
+        JPAQueryFactory queryFactory = new JPAQueryFactory(entityManager);
+        QMember member = QMember.member;
+        JPAQuery<Member> query = queryFactory.selectFrom(member)
+                /*.orderBy(new OrderSpecifier(
+                        Order.valueOf(Order.ASC.name()),
+                        new PathBuilder(Member.class, "id")))*/
+//                .limit(pageRequest.getPageSize())
+//                .offset(pageRequest.getPageNumber())
+                .where(member.username.like("%" + memberDto.getUsername() + "%"));
+
+//        long total = query.fetchCount();
+//        List<MemberDto> members= query.fetch().stream().map(MemberDto::maptoMemberDto).toList();
+//        return new PageImpl<>(members,pageRequest,total);
+
+
         Page<Member> stakingPlanDetailPage = memberRepository.findAll(predicate, memberDto.getPageQueryModel().getPageable());
         return stakingPlanDetailPage.map(MemberDto::maptoMemberDto);
     }
