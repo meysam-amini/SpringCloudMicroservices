9f6a31c0b2db3155f28b2225133142028d448322
m.amini <m.amini@ernyka.com>
2023-08-28 22:08:18 +0330

    circuitBreaker on api-gateway problem

--- api-gateway/src/main/java/com/meysam/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/meysam/apigateway/ApiGatewayApplication.java
@@ -6,6 +6,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+//@EnableHystrix
+//@ConditionalOnClass({ DispatcherHandler.class,
+//        ReactiveResilience4JAutoConfiguration.class,
+//        HystrixCircuitBreakerAutoConfiguration.class })
 public class ApiGatewayApplication {
 
     public static void main(String[] args) {
@@ -24,5 +28,14 @@ public class ApiGatewayApplication {
                         .circuitBreakerConfig(circuitBreakerRegistry.getConfiguration("backendB").orElse(circuitBreakerRegistry.getDefaultConfig())),
                 "backendB");
         return reactiveResilience4JCircuitBreakerFactory;
+    }*/
+
+    /*@Bean
+    public Customizer<Resilience4JCircuitBreakerFactory> defaultCustomizer(){
+        return factory -> factory.configureDefault(
+                id -> new Resilience4JConfigBuilder(id)
+                        .circuitBreakerConfig(CircuitBreakerConfig.ofDefaults())
+                        .build()
+        );
     }*/
 }
--- api-gateway/src/main/java/com/meysam/apigateway/controller/GatewayFallback.java
+++ api-gateway/src/main/java/com/meysam/apigateway/controller/GatewayFallback.java
@@ -1,29 +1,20 @@
 package com.meysam.apigateway.controller;
 
-import com.meysam.apigateway.model.FallbackResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 
+@Slf4j
 @RestController
 @RequestMapping("/fallback")
 public class GatewayFallback {
 
-    @GetMapping("/testService")
-    public FallbackResponse getFallBackBackendA() {
-        FallbackResponse a = new FallbackResponse();
-        a.setMsgCode(500);
-        a.setMsg("fallBackMsg");
-        return a;
-    }
 
-
-    @PostMapping("/testInternalServiceError")
-    public FallbackResponse getFallBackBackendB() {
-        FallbackResponse a = new FallbackResponse();
-        a.setMsgCode(500);
-        a.setMsg("fallBackMsg");
-        return a;
+    @GetMapping("/testInternalServiceError")
+    Flux<Void> getFallBackBackendB() {
+        log.info("Fallback for book service");
+        return Flux.empty();
     }
 }
