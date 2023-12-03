2b5b5078c4d19c1ff8813710ec0de0a65ac22ca8
meysam-amini <meysamamini7394@gmail.com>
2023-12-03 23:55:46 +0330

    add PermissionController

---
+++ backoffice/backoffice-webapi/src/main/java/com/meysam/backoffice/webapi/controller/members/PermissionController.java
@@ -0,0 +1,29 @@
+package com.meysam.backoffice.webapi.controller.members;
+
+import com.meysam.backoffice.service.auth.api.AdminAuthService;
+import com.meysam.common.model.dto.RegisterAdminRequestDto;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/protected")
+@RequiredArgsConstructor
+public class PermissionController {
+
+    private final AdminAuthService adminAuthService;
+
+
+    @PostMapping(value = "register", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @PreAuthorize("hasAnyAuthority('PERMISSION_regiter_new_admin')")
+    public ResponseEntity registerNewAdmin(@Valid @RequestBody RegisterAdminRequestDto registerAdminRequestDto) {
+        return adminAuthService.register(registerAdminRequestDto);
+    }
+
+}
