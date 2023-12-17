c77a38ed01108fdd5da669c405d0288a992129dd
meysam-amini <meysamamini7394@gmail.com>
2023-12-17 18:39:53 +0330

    direct and role permission assign

--- backoffice/backoffice-webapi/src/main/java/com/meysam/backoffice/webapi/controller/members/AdminProtectedController.java
+++ backoffice/backoffice-webapi/src/main/java/com/meysam/backoffice/webapi/controller/members/AdminProtectedController.java
@@ -3,6 +3,8 @@ package com.meysam.backoffice.webapi.controller.members;
 import com.meysam.backoffice.service.auth.api.AdminAuthService;
 import com.meysam.common.customsecurity.model.SecurityPrinciple;
 import com.meysam.common.customsecurity.model.constants.SessionConstants;
+import com.meysam.common.customsecurity.model.dto.AssignDirectPermissionDto;
+import com.meysam.common.customsecurity.model.dto.AssignRolePermissionDto;
 import com.meysam.common.customsecurity.service.api.PermissionService;
 import com.meysam.common.customsecurity.model.dto.AddPermissionDto;
 import com.meysam.common.customsecurity.model.dto.RegisterAdminRequestDto;
@@ -37,6 +39,18 @@ public class AdminProtectedController {
     @PreAuthorize("hasAnyAuthority('PERMISSION_ADD_NEW_PERMISSION')")
     public ResponseEntity addNewPermission(@Valid @RequestBody AddPermissionDto addPermissionDto) {
         return ResponseEntity.ok(permissionService.addPermission(addPermissionDto));
+    }
+
+    @PostMapping(value = "assign-direct-permission", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @PreAuthorize("hasAnyAuthority('PERMISSION_ADD_NEW_PERMISSION')")
+    public ResponseEntity assignDirectPermission(@Valid @RequestBody AssignDirectPermissionDto directPermissionDto) {
+        return ResponseEntity.ok(permissionService.assignPermissionToUsername(directPermissionDto));
+    }
+
+    @PostMapping(value = "assign-role-permission", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @PreAuthorize("hasAnyAuthority('PERMISSION_ADD_NEW_PERMISSION')")
+    public ResponseEntity assignRolePermission(@Valid @RequestBody AssignRolePermissionDto rolePermissionDto) {
+        return ResponseEntity.ok(permissionService.assignPermissionToRole(rolePermissionDto));
     }
 
 
--- common/common-custom-security/src/main/java/com/meysam/common/customsecurity/model/dto/AssignRolePermissionDto.java
+++ common/common-custom-security/src/main/java/com/meysam/common/customsecurity/model/dto/AssignRolePermissionDto.java
@@ -1,5 +1,6 @@
 package com.meysam.common.customsecurity.model.dto;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 @NoArgsConstructor
@@ -7,6 +8,8 @@ import lombok.*;
 @Getter@Setter@Builder
 public class AssignRolePermissionDto {
 
+    @NotNull(message = "invalid permission code")
     private String permissionCode;
+    @NotNull(message = "invalid role code")
     private String roleCode;
 }
