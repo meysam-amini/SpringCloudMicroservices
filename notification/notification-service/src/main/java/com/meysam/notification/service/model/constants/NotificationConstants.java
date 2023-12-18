a3a2c8999f10f9be470b206a550dd08715310fd4
M.Amini <meysamamini7394@gmail.com>
2023-12-18 09:32:28 +0330

    sms provider request/response dtos

---
+++ notification/notification-service/src/main/java/com/meysam/notification/service/model/constants/NotificationConstants.java
@@ -0,0 +1,29 @@
+package com.meysam.notification.service.model.constants;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+public class NotificationConstants {
+
+    public enum NotificationType {
+        EMAIL,
+        SMS
+    }
+
+    public enum NotificationDestinationGroup {
+        COMPLAINING,
+        COMPLAINANT,
+        REPRESENTATIVE
+    }
+
+    @Getter
+    @AllArgsConstructor
+    public enum NotificationMessages {
+
+        _01("msg sent to:{}"),
+        ;
+        private String text;
+
+    }
+
+}
---
+++ notification/notification-service/src/main/java/com/meysam/notification/service/model/dto/SMSResponseMessageDto.java
@@ -0,0 +1,13 @@
+package com.meysam.notification.service.model.dto;
+
+import lombok.*;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class SMSResponseMessageDto {
+    private int status;
+    //response fields here
+}
