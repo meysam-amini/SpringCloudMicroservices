df0ff532d5ccd71161754ad274d163bef1275c1a
M.Amini <meysamamini7394@gmail.com>
2024-01-07 20:57:00 +0330

    OutboxService

---
+++ common/outbox/src/main/java/com/meysam/common/outbox/service/OutboxService.java
@@ -0,0 +1,19 @@
+package com.meysam.common.outbox.service;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.taba.common.model.model.entity.notification.OutBox;
+import org.taba.common.model.model.enums.OutboxEventStatus;
+
+import java.util.List;
+
+public interface OutboxService<T extends OutBox> {
+
+    void process();
+
+    Page<T> findAllFailed(Pageable pageable);
+
+    int updateStatusInDistinctTransaction (Long id, OutboxEventStatus status, List<OutboxEventStatus> validStatuses);
+
+    void retry(T t);
+}
