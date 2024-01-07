1d5b126e3c10e456835d1751e4695aca28bfea0b
M.Amini <meysamamini7394@gmail.com>
2024-01-07 20:54:58 +0330

    add OutBox entity

---
+++ common/outbox/src/main/java/com/meysam/common/outbox/model/entity/OutBox.java
@@ -0,0 +1,36 @@
+package com.meysam.common.outbox.model.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.util.Date;
+
+@Getter
+@MappedSuperclass
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class OutBox {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "STATUS")
+    private OutboxEventStatus status=OutboxEventStatus.UNSENT;
+
+    @Column(name = "CREATEDDATE")
+    @CreationTimestamp
+    private Date createdDate = new Date();
+
+    @Column(name = "TRACKINGCODE")
+    private Integer trackingCode;
+
+    @Column(name = "RETRYCOUNT")
+    private Integer retryCount;
+}
