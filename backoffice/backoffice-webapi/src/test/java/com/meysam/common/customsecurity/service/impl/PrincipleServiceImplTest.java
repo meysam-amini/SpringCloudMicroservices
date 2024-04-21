d4c278e4621e3bc74bb95541d7d1d55a9a0404b3
meysam-amini <meysamamini7394@gmail.com>
2024-04-21 21:50:19 +0330

    PrincipleServiceImpl

---
+++ backoffice/backoffice-webapi/src/test/java/com/meysam/common/customsecurity/service/impl/PrincipleServiceImplTest.java
@@ -0,0 +1,16 @@
+package com.meysam.common.customsecurity.service.impl;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PrincipleServiceImplTest {
+
+    @Test
+    void getSecurityPrinciple() {
+    }
+
+    @Test
+    void removeSession() {
+    }
+}
