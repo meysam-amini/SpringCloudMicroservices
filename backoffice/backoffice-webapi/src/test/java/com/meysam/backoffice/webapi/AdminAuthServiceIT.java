e18a47aacaa4ac5bee4278e959976709b8be4faf
meysam-amini <meysamamini7394@gmail.com>
2024-10-04 00:45:39 +0330

    AdminAuthServiceIT

--- backoffice/backoffice-webapi/src/test/java/com/meysam/backoffice/webapi/AdminAuthServiceIT.java
+++ backoffice/backoffice-webapi/src/test/java/com/meysam/backoffice/webapi/AdminAuthServiceIT.java
@@ -9,7 +9,6 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.ResponseEntity;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.PostgreSQLContainer;
@@ -18,7 +17,7 @@ import org.testcontainers.utility.DockerImageName;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
-public class AdminControllerIT {
+public class AdminAuthServiceIT {
 
     @Autowired
     private  AdminAuthService adminAuthService;
