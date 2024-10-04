22d0d161fc95e5fb1eb69296cb77e5f82d5f727e
meysam-amini <meysamamini7394@gmail.com>
2024-10-04 05:14:43 +0330

    add log aspect

---
+++ backoffice/backoffice-webapi/src/main/java/com/meysam/backoffice/webapi/config/aspect/log/LogAspect.java
@@ -0,0 +1,50 @@
+package com.meysam.backoffice.webapi.config.aspect.log;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.data.repository.Repository;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+
+@Slf4j
+@Aspect
+@Component
+public class LogAspect {
+
+
+    @Around(value = "@annotation(org.taba.backoffice.config.aspect.log.annotation.MethodLog)")
+    public Object loggerPointCut(ProceedingJoinPoint joinPoint) throws Throwable {
+        long trackingCode = System.identityHashCode(joinPoint.getArgs());
+        String methodName = joinPoint.getSignature().getName();
+        Class<?> clazz = joinPoint.getTarget().getClass();
+        if (Repository.class.isAssignableFrom(clazz)) {
+            clazz = Arrays.stream(joinPoint.getTarget().getClass().getInterfaces()).findFirst().orElse(clazz);
+        }
+        String className = clazz.getName();
+
+        log.info("calling '{}' method on '{}' class  is starting with following arguments: {}",
+                methodName, className, serializeArguments(/*serializer,*/ joinPoint.getArgs()));
+
+        try {
+            Object result = joinPoint.proceed();
+            log.info("calling '{}' method on '{}' class is done", methodName, className);
+            return result;
+        } catch (Exception exception) {
+            log.info("calling '{}' method on '{}' class encounter exception, the exception is : {}", methodName, className, exception);
+            throw exception;
+        }
+    }
+
+    public static String serializeArguments(Object[] args) {
+        try {
+            return Arrays.toString(args);
+        } catch (Throwable exception) {
+            log.error("ERROR in serializing arguments", exception);
+            return "$ERROR_SERIALIZING_ARGUMENTS$" + exception.getMessage();
+        }
+    }
+}
