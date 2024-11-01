f1c368bfe7a2789a2e664564659d638925ead079
meysam-amini <meysamamini7394@gmail.com>
2024-11-01 20:25:56 +0330

    Serialize SecurityPrinciple

--- common/common-custom-security/src/main/java/com/meysam/common/customsecurity/model/SecurityPrinciple.java
+++ common/common-custom-security/src/main/java/com/meysam/common/customsecurity/model/SecurityPrinciple.java
@@ -3,11 +3,12 @@ package com.meysam.common.customsecurity.model;
 import lombok.Builder;
 import lombok.Data;
 
+import java.io.Serializable;
 import java.util.List;
 
 @Data
 @Builder
-public class SecurityPrinciple {
+public class SecurityPrinciple implements Serializable {
     private List<String> roles;
     private List<String> permissions;
     private String username;
